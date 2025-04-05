1ce9653e63ac00553b81148716e5690ebe1f8401
raf <rraf@tuta.io>
2025-04-05 14:45:01 +0100

    update: initialised nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,34 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Page() {
     return (
-        <View style={styles.container}>
-            <View style={styles.main}>
-                <Text style={styles.title}>Hello World</Text>
-                <Text style={styles.subtitle}>This is the first page of your app.</Text>
-            </View>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-[64px] font-bold">Index</Text>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        padding: 24,
-    },
-    main: {
-        flex: 1,
-        justifyContent: "center",
-        maxWidth: 960,
-        marginHorizontal: "auto",
-    },
-    title: {
-        fontSize: 64,
-        fontWeight: "bold",
-    },
-    subtitle: {
-        fontSize: 36,
-        color: "#38434D",
-    },
-});
