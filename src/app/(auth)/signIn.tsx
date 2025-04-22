bc0e8eac66bb4c2d4119af98ea9fb863becc1385
raf <rraf@tuta.io>
2025-04-22 14:54:48 +0100

    update: setup sign in ui

--- src/app/(auth)/signIn.tsx
+++ src/app/(auth)/signIn.tsx
@@ -1,6 +1,9 @@
 import { VideoBackground } from "@/components/VideoBackground";
-import { Stack } from "expo-router";
-import { StatusBar, View } from "react-native";
+import { Link, Stack } from "expo-router";
+import { Pressable, StatusBar, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import colors from "tailwindcss/colors";
+import Logo from "~/assets/images/hinge-logo.svg";
 
 export default function Page() {
     return (
@@ -11,7 +14,24 @@ export default function Page() {
                     headerShown: false,
                 }}
             />
-            <VideoBackground source={require("~/assets/images/background.mp4")} />
+            <VideoBackground source={require("~/assets/images/background.mp4")}>
+                <SafeAreaView className="flex-1 p-10">
+                    <View className="flex-1 items-center pt-20">
+                        <Logo fill={colors.white} width={150} height={60} />
+                        <View className="h-4" />
+                        <Text className="text-white text-xl font-poppins-semibold">
+                            Get ready for the next battle
+                        </Text>
+                    </View>
+                    <Link href={"/phone"} asChild>
+                        <Pressable className="bg-black rounded-full p-4 flex-row items-center justify-center">
+                            <Text className="text-white text-lg font-poppins-semibold">
+                                Register with Phone Number
+                            </Text>
+                        </Pressable>
+                    </Link>
+                </SafeAreaView>
+            </VideoBackground>
         </View>
     );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,25 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "../../global.css";
 import { cssInterop } from "nativewind";
 import { VideoView } from "expo-video";
+import { fonts } from "@/constants/fonts";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
 
 cssInterop(VideoView, { className: { target: "style" } });
 
 export default function Layout() {
+    const [loaded, error] = useFonts(fonts);
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
     return (
         <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="(app)" />
