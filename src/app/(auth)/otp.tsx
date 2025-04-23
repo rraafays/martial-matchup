668400b5bd796eafc7be3826d17af14b88a04449
raf <rraf@tuta.io>
2025-04-23 13:41:27 +0100

    update: wrote one time password page

--- src/app/(auth)/otp.tsx
+++ src/app/(auth)/otp.tsx
@@ -1,9 +1,65 @@
-import { Text, View } from "react-native";
+import { ForwardActionButton } from "@/components/ForwardActionButton";
+import { StackHeader } from "@/components/StackHeader";
+import { router } from "expo-router";
+import { useMemo, useState } from "react";
+import { StatusBar, Text, TextInput, View, KeyboardAvoidingView, Platform } from "react-native";
+import colors from "tailwindcss/colors";
 
 export default function Page() {
+    const [oneTimePassword, setOneTimePassword] = useState("");
+
+    const handleOneTimePasswordChange = (text: string) => {
+        setOneTimePassword(text);
+    };
+
+    const isValidOneTimePassword = useMemo(() => {
+        return oneTimePassword.length === 6;
+    }, [oneTimePassword]);
+
+    const handleSubmit = () => {};
+
     return (
-        <View>
-            <Text>Otp</Text>
-        </View>
+        <KeyboardAvoidingView
+            className="flex-1 bg-white p-5"
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={100}
+        >
+            <StatusBar barStyle="dark-content" />
+            <StackHeader />
+            <View className="flex-1 justify-center pt-10">
+                <View className="flex-1">
+                    <Text className="text-4xl font-poppins-semibold">
+                        Enter your verification code
+                    </Text>
+                    <View className="h-20" />
+                    <View className="flex-row gap-2 h-15">
+                        {Array.from({ length: 6 }).map((_, index) => (
+                            <View key={index} className="border-b flex-1">
+                                <Text className="text-4xl font-poppins-medium items-center justify-center text-center">
+                                    {oneTimePassword[index] || ""}
+                                </Text>
+                            </View>
+                        ))}
+                    </View>
+                    <TextInput
+                        className="absolute top-0 left-0 opacity-0"
+                        style={{ lineHeight: undefined }}
+                        selectionColor={colors.black}
+                        keyboardType="numeric"
+                        textContentType="oneTimeCode"
+                        autoFocus={true}
+                        value={oneTimePassword}
+                        onChangeText={handleOneTimePasswordChange}
+                        maxLength={6}
+                    />
+                </View>
+                <View className="items-end">
+                    <ForwardActionButton
+                        disabled={!isValidOneTimePassword}
+                        onPress={handleSubmit}
+                    />
+                </View>
+            </View>
+        </KeyboardAvoidingView>
     );
 }
--- src/app/(auth)/phone.tsx
+++ src/app/(auth)/phone.tsx
@@ -1,12 +1,13 @@
 import { ForwardActionButton } from "@/components/ForwardActionButton";
 import { StackHeader } from "@/components/StackHeader";
-import { router } from "expo-router";
-import { useMemo, useState } from "react";
+import { router, useFocusEffect } from "expo-router";
+import { useMemo, useRef, useState } from "react";
 import { StatusBar, Text, TextInput, View, KeyboardAvoidingView, Platform } from "react-native";
 import colors from "tailwindcss/colors";
 
 export default function Page() {
     const [phoneNumber, setPhoneNumber] = useState("");
+    const phoneNumberRef = useRef<TextInput>(null);
 
     const handlePhoneNumberChange = (text: string) => {
         setPhoneNumber(text);
@@ -20,6 +21,10 @@ export default function Page() {
         router.push({ pathname: "/otp", params: { phoneNumber } });
     };
 
+    useFocusEffect(() => {
+        phoneNumberRef.current?.focus();
+    });
+
     return (
         <KeyboardAvoidingView
             className="flex-1 bg-white p-5"
@@ -31,19 +36,19 @@ export default function Page() {
             <View className="flex-1 justify-center pt-10">
                 <View className="flex-1">
                     <Text className="text-4xl font-poppins-semibold">Enter your phone number</Text>
-                    <View className="h-20 mt-10">
-                        <TextInput
-                            className="border-b h-20 text-4xl font-poppins-medium"
-                            style={{ lineHeight: undefined }}
-                            selectionColor={colors.black}
-                            keyboardType="phone-pad"
-                            textContentType="telephoneNumber"
-                            autoFocus={true}
-                            value={phoneNumber}
-                            onChangeText={handlePhoneNumberChange}
-                            maxLength={16}
-                        />
-                    </View>
+                    <View className="h-20" />
+                    <TextInput
+                        className="border-b h-20 text-4xl font-poppins-medium"
+                        style={{ lineHeight: undefined }}
+                        selectionColor={colors.black}
+                        keyboardType="phone-pad"
+                        textContentType="telephoneNumber"
+                        autoFocus={true}
+                        value={phoneNumber}
+                        onChangeText={handlePhoneNumberChange}
+                        maxLength={16}
+                        ref={phoneNumberRef}
+                    />
                 </View>
                 <View className="items-end">
                     <ForwardActionButton disabled={!isValidPhoneNumber} onPress={handleSubmit} />
