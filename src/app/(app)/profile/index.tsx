5f5436ab7fbfd45a7c11fb6da8dd66cf06ac0d82
raf <rraf@tuta.io>
2025-04-26 20:22:45 +0100

    update: wrote details

--- src/app/(app)/profile/index.tsx
+++ src/app/(app)/profile/index.tsx
@@ -1,6 +1,8 @@
+import { List } from "@/components/list";
 import { PhotoGrid } from "@/components/photoGrid";
 import { useEdit } from "@/store/edit";
 import { ScrollView, Text, View } from "react-native";
+import { details } from "@/utils/details";
 
 export default function Profile() {
     const { edits, gridActive } = useEdit();
@@ -8,7 +10,7 @@ export default function Profile() {
     if (!edits) {
         return (
             <View className="flex-1 justify-center items-center bg-white">
-                <Text>something</Text>
+                <Text>Something went wrong.</Text>
             </View>
         );
     }
@@ -22,6 +24,10 @@ export default function Profile() {
             <View className="px-5">
                 <Text className="text-base font-poppins-semibold mb-2">Showcase</Text>
                 <PhotoGrid profile={edits} />
+                <View className="h-10" />
+            </View>
+            <View className="px-5">
+                <List title="Details" data={details} profile={edits} />
             </View>
         </ScrollView>
     );
---
+++ src/utils/details.ts
@@ -0,0 +1,54 @@
+import { PrivateProfile } from "@/api/my-profile/types";
+import { age } from "@/utils/age";
+
+export const details = [
+    {
+        title: "Name",
+        getValue: (profile: PrivateProfile) => {
+            return profile?.name || "None";
+        },
+        route: "/profile/name",
+    },
+    {
+        title: "Age",
+        getValue: (profile: PrivateProfile) => {
+            return profile?.date_of_birth ? age(profile?.date_of_birth) : "None";
+        },
+        route: "/profile/age",
+    },
+    {
+        title: "Height",
+        getValue: (profile: PrivateProfile) => {
+            return profile?.height_cm ? profile?.height_cm + " cm" : "None";
+        },
+        route: "/profile/height",
+    },
+    {
+        title: "Location",
+        getValue: (profile: PrivateProfile) => {
+            return profile?.neighborhood || "None";
+        },
+        route: "/profile/location",
+    },
+    {
+        title: "Fighting Style",
+        getValue: (profile: PrivateProfile) => {
+            return profile?.fighting_style?.name || "None";
+        },
+        route: "/profile/fighting-style",
+    },
+    {
+        title: "Fight Type",
+        getValue: (profile: PrivateProfile) => {
+            return profile?.fight_type?.name || "None";
+        },
+        route: "/profile/fight-type",
+    },
+    {
+        title: "Years of Experience",
+        getValue: (profile: PrivateProfile) => {
+            return profile?.years_of_experience.toString() || "None";
+        },
+        route: "/profile/experience",
+    },
+];
