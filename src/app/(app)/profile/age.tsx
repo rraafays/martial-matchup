baa91f1d1ebde93d6e35f85afe4ca8e596073f7a
raf <rraf@tuta.io>
2025-04-27 10:31:41 +0100

    update: wrote age entry

--- src/app/(app)/profile/age.tsx
+++ src/app/(app)/profile/age.tsx
@@ -1,7 +1,57 @@
-import { Text, View } from "react-native";
+import { PrivateProfile } from "@/api/my-profile/types";
+import { StackHeaderV4 } from "@/components/StackHeaderV4";
+import { useEdit } from "@/store/edit";
+import { age } from "@/utils/age";
+import DateTimePicker, { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+import { subYears } from "date-fns";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Platform, Text, View } from "react-native";
 
 export default function Page() {
-    <View>
-        <Text>Age</Text>
-    </View>;
+    const { edits, setEdits } = useEdit();
+    const [date, setDate] = useState(edits?.date_of_birth || subYears(new Date(), 18));
+    const [show, setShow] = useState(true);
+
+    const onChange = (_event: DateTimePickerEvent, selectedDate: Date | undefined) => {
+        if (selectedDate) {
+            setDate(selectedDate);
+        }
+
+        if (Platform.OS === "android") {
+            setShow(false);
+        }
+    };
+
+    const handlePress = () => {
+        if (date) {
+            setEdits({
+                ...edits,
+                dob: date as string,
+            } as PrivateProfile);
+        }
+        router.back();
+    };
+
+    return (
+        <View className="flex-1 bg-white p-5">
+            <StackHeaderV4 title="Age" onPressBack={handlePress} />
+            {(show || Platform.OS === "ios") && (
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={new Date(date)}
+                    mode={"date"}
+                    is24Hour={true}
+                    onChange={onChange}
+                    display="spinner"
+                    maximumDate={subYears(new Date(), 18)}
+                    minimumDate={subYears(new Date(), 100)}
+                />
+            )}
+            <Text
+                className="text-4xl text-center font-poppins-medium mt-5"
+                onPress={() => setShow(true)}
+            >{`Age ${age(date.toString())}`}</Text>
+        </View>
+    );
 }
